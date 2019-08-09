a400ab00148a8415f1e718704d9ebecbec4767e8
Leenamkyu <dlskarb032@naver.com>
2019-08-09 14:03:58 +0900

    Update Companion

--- TMI/AppCompanion/forms.py
+++ TMI/AppCompanion/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Companion
 
+#새로추가 8/9        
+class DateForm(forms.Form):
+    date = forms.DateField(input_formats=['%d/%m/%Y %H:%M'])       
+
 class CompanionPost(forms.ModelForm):
     class Meta:
         model = Companion
-        fields = ['status','category','title', 'country', 'city', 'bucket_list', 'body']
+        start_date = forms.DateField(input_formats=['%m/%d/%Y'])
+        end_date = forms.DateField(input_formats=['%m/%d/%Y'])
+        fields = ['status','category','title', 'country', 'city', 'bucket_list', 'start_date','end_date','body'] #새로 추가 
+       
