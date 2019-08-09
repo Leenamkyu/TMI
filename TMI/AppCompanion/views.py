7aa8d0a72a5ea5f3212edd4d2b500d87b1a1db2f
Leenamkyu <dlskarb032@naver.com>
2019-08-09 23:58:54 +0900

    Update namkyu

--- TMI/AppCompanion/views.py
+++ TMI/AppCompanion/views.py
@@ -45,10 +45,8 @@ def search(request):
     q_body = request.GET.get('body','')
     q_category = request.GET.get('category','')
 
-    #새로추가 8/9######
     q_start_date = request.GET.get('start_date','')
     q_end_date = request.GET.get('end_date','')
-    ###########
 
     #사용자가 입력한 값이 있는지 확인 후 순차적으로 필터링을 거침
     if q_category:
@@ -62,15 +60,26 @@ def search(request):
     if q_body:
         qs = qs.filter(body = q_body)
 
-    ######새로추가 8/9########## => 미완성: 시작날짜보다 같거나 크면 filter, 마지막일보다 작거나 같으면 filter 하도록 수정하기(일부 수정완료 - 조건 다시 확인하기)    
-    if q_start_date:
-        q_start_date = datetime.datetime.strptime(q_start_date, '%m/%d/%Y') # '/'로 나뉘어진 문자열 형태의 날짜를 dateField type으로 변환    
-        qs = qs.filter(end_date__gte = q_start_date)
+    ######새로추가 8/9########## => 대략 완료. 테스트 필요 
+    # if q_start_date:
+    #     q_start_date = datetime.datetime.strptime(q_start_date, '%m/%d/%Y') # '/'로 나뉘어진 문자열 형태의 날짜를 dateField type으로 변환    
+    #     qs = qs.filter(end_date__gte = q_start_date)
 
-    if q_end_date:
-        q_end_date = datetime.datetime.strptime(q_end_date, '%m/%d/%Y')
-        qs = qs.filter(end_date__lte = q_end_date)
-    ###########        
+    # if q_end_date:
+    #     q_end_date = datetime.datetime.strptime(q_end_date, '%m/%d/%Y')
+    #     qs = qs.filter(end_date__lte = q_end_date)
+
+    if q_start_date: 
+        q_start_date = datetime.datetime.strptime(q_start_date, '%m/%d/%Y')
+        qs = qs.filter(end_date__gte = q_start_date)
+        
+        if q_end_date:
+            q_end_date = datetime.datetime.strptime(q_end_date, '%m/%d/%Y')
+            qs = qs.filter(start_date__lte = q_end_date)
+    else:
+        if q_end_date:
+            q_end_date = datetime.datetime.strptime(q_end_date, '%m/%d/%Y')
+            qs = qs.filter(start_date__gte = q_end_date)       
     
     #필터링 된 객체 리스트와 검색값 반환
     return render(request, 'Companion_search.html', {'result_list': qs, 'search': q_country})
@@ -88,11 +97,8 @@ def update(request, pk):
             companion.city = form.cleaned_data['city']
             companion.bucket_list = form.cleaned_data['bucket_list']
             companion.body = form.cleaned_data['body']
-
-            ####새로추가 8/9    
             companion.start_date = form.cleaned_data['start_date']
             companion.end_date = form.cleaned_data['end_date']
-            #######
 
             companion.save()
             return redirect('companion_main')
